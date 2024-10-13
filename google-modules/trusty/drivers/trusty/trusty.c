e4c80d823bd589ae85b67e3f522e4102eb71fc0a
Sultan Alsawaf <sultan@kerneltoast.com>
2024-10-12 21:01:07 -0700

    trusty: Don't disable preemption in nop kthread
    A lot of time is spent in here with preemption disabled, hurting system-
    wide latency. It's not necessary since the nop kthreads are pinned to their
    respective CPUs, so any per-CPU data accessed in a nop kthread is safe
    without disabling preemption.
    
    Don't disable preemption in the nop kthread to improve system-wide latency.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/trusty/drivers/trusty/trusty.c
+++ google-modules/trusty/drivers/trusty/trusty.c
@@ -961,8 +961,6 @@ static void nop_work_func(struct trusty_work *tw)
 		if (kthread_should_park())
 			kthread_parkme();
 
-		preempt_disable();
-
 		if (tw != this_cpu_ptr(s->nop_works)) {
 			dev_warn_ratelimited(s->dev,
 					     "trusty-nop-%d ran on wrong cpu, %u\n",
@@ -991,8 +989,6 @@ static void nop_work_func(struct trusty_work *tw)
 				do_nop = true;
 			}
 		}
-
-		preempt_enable();
 	}
 	dev_dbg(s->dev, "%s: done\n", __func__);
 }
