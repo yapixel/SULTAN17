51199eb5da6d6d30c106005d73dd256ac375666b
Sultan Alsawaf <sultan@kerneltoast.com>
2025-06-15 00:09:14 -0700

    treewide: Fix GCC compile warnings
    Fix mostly trivial compile warnings emitted by GCC.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/bms/p9221_charger.h
+++ google-modules/bms/p9221_charger.h
@@ -1238,10 +1238,6 @@ enum p9xxx_renego_state {
 
 #define UEVENT_ENVP_LEN 20
 
-static char *uevent_source_str[] = {
-	"WLC", "FAN", "RTX"
-};
-
 enum uevent_source {
 	UEVENT_WLC = 0,
 	UEVENT_FAN,
