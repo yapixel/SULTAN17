d8bcfc64646d21db3cc861586d1e84d7562f0340
Andreas Tillman <ante00@gmail.com>
2026-07-17 09:13:07 +0200

    update patch_sultan.sh

--- patch_sultan.sh
+++ patch_sultan.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 get_script_dir()
 {
     local SOURCE_PATH="${BASH_SOURCE[0]}"
@@ -22,72 +24,89 @@ get_script_dir()
     echo "$SCRIPT_DIR"
 }
 
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <gs201|zuma|zumapro>"
+    exit 1
+fi
+
+case "$1" in
+    gs201|zuma|zumapro)
+        TARGET="$1"
+        ;;
+    *)
+        echo "Error: '$1' is not a valid target."
+        echo "Valid targets are: gs201, zuma, zumapro"
+        exit 1
+        ;;
+esac
+
 export KERNEL_REPO="$(get_script_dir)"
 
 cd $KERNEL_REPO
 
 ##PRE-PATCHING##
 #KSU
-if grep -q CONFIG_KSU=y $KERNEL_REPO/arch/arm64/configs/zumapro_defconfig; then
+if grep -q "CONFIG_KSU=y" "$KERNEL_REPO/arch/arm64/configs/${TARGET}_defconfig"; then
 echo "defconfig already patched with KSU"
 else
 echo "Patching defconfig with CONFIG_KSU=y"
-echo CONFIG_KSU=y >> $KERNEL_REPO/arch/arm64/configs/zumapro_defconfig
+echo "CONFIG_KSU=y" >> "$KERNEL_REPO/arch/arm64/configs/${TARGET}_defconfig"
 fi
 
 #SUS_SU
-if grep -q CONFIG_KSU_SUSFS_SUS_SU=n $KERNEL_REPO/arch/arm64/configs/zumapro_defconfig; then
+if grep -q "CONFIG_KSU_SUSFS_SUS_SU=n" "$KERNEL_REPO/arch/arm64/configs/${TARGET}_defconfig"; then
 echo "defconfig already patched with KSU_SUFS_SUS_SU=n"
 else
 echo "Patching defconfig with CONFIG_KSU_SUSFS_SUS_SU=n"
-echo CONFIG_KSU_SUSFS_SUS_SU=n >> $KERNEL_REPO/arch/arm64/configs/zumapro_defconfig
+echo "CONFIG_KSU_SUSFS_SUS_SU=n" >> "$KERNEL_REPO/arch/arm64/configs/${TARGET}_defconfig"
 fi
 
 #SUSFS
-if grep -q CONFIG_KSU_SUSFS=y $KERNEL_REPO/arch/arm64/configs/zumapro_defconfig; then
+if grep -q "CONFIG_KSU_SUSFS=y" "$KERNEL_REPO/arch/arm64/configs/${TARGET}_defconfig"; then
 echo "defconfig already patched with SUSFS"
 else
 echo "Patching defconfig with CONFIG_KSU_SUSFS=y"
-echo CONFIG_KSU_SUSFS=y >> $KERNEL_REPO/arch/arm64/configs/zumapro_defconfig
+echo "CONFIG_KSU_SUSFS=y" >> "$KERNEL_REPO/arch/arm64/configs/${TARGET}_defconfig"
 fi
 
-if grep -q CONFIG_COMPAT=y $KERNEL_REPO/arch/arm64/configs/zumapro_defconfig; then
+if grep -q "CONFIG_COMPAT=y" "$KERNEL_REPO/arch/arm64/configs/${TARGET}_defconfig"; then
 echo "defconfig already patched with COMPAT"
 else
 echo "Patching defconfig with CONFIG_COMPAT=y (fixes leaking symbols)"
-echo CONFIG_COMPAT=y >> $KERNEL_REPO/arch/arm64/configs/zumapro_defconfig
+echo "CONFIG_COMPAT=y" >> "$KERNEL_REPO/arch/arm64/configs/${TARGET}_defconfig"
 fi
 
 ##CLONE KERNELSU AND SUSFS##
-cd $KERNEL_REPO
+cd "$KERNEL_REPO"
 #fetch ksu
 curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s main
 
 ##fetch anykernel
-git clone https://github.com/Ante0/AnyKernel3 -b sultan-17-caimito AnyKernel
+git clone --depth=1 https://github.com/Ante0/AnyKernel3 -b sultan-17-caimito AnyKernel
 
 #fetch susfs
 git clone https://gitlab.com/simonpunk/susfs4ksu -b gki-android14-6.1 --depth=1
 #copy files
-cd $KERNEL_REPO/susfs4ksu/
+cd "$KERNEL_REPO/susfs4ksu/"
 
-cp ./kernel_patches/50_add_susfs_in_gki-android14-6.1.patch $KERNEL_REPO/
-cp ./kernel_patches/KernelSU/10_enable_susfs_for_ksu.patch $KERNEL_REPO/KernelSU/
-cp ./kernel_patches/fs/* $KERNEL_REPO/fs/
-cp ./kernel_patches/include/linux/* $KERNEL_REPO/include/linux/
+cp "./kernel_patches/50_add_susfs_in_gki-android14-6.1.patch" "$KERNEL_REPO/"
+cp "./kernel_patches/KernelSU/10_enable_susfs_for_ksu.patch" "$KERNEL_REPO/KernelSU/"
+cp "./kernel_patches/fs/*" "$KERNEL_REPO/fs/"
+cp "./kernel_patches/include/linux/*" "$KERNEL_REPO/include/linux/"
 
 ##PATCHING##
-cd $KERNEL_REPO
+cd "$KERNEL_REPO"
 patch -p1 < 50_add_susfs_in_gki-android14-6.1.patch
 
-cd $KERNEL_REPO/KernelSU/
+cd "$KERNEL_REPO/KernelSU/"
 patch -p1 < 10_enable_susfs_for_ksu.patch
 
 echo "Patching utf8"
-cd $KERNEL_REPO
+cd "$KERNEL_REPO"
 patch -p1 < utf8.patch
 
 echo "fixing Sultan rejects (fs/open.c, fs/namespace.c and kernel/sys.c"
+cd "$KERNEL_REPO"
 patch -p1 < fixer.patch
 
 echo "done!"
