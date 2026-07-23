806cf79421f3a43609bf4f89cf8954412c328b7c
Ante0 <ante00@gmail.com>
2026-07-23 11:01:41 +0200

    Update patch_sultan.sh
    DAMNED TYPOS

--- gscripts/patch_sultan.sh
+++ gscripts/patch_sultan.sh
@@ -154,9 +154,9 @@ patch_susfs_ksu_next() {
 
 	msg "Applying SUSFS KernelSU-Next integration"
 
-        apply_patch_optional \
-                "$KERNEL_REPO/KernelSU-Next" \
-                "$KERNEL_REPO/susfs4ksu/kernel_patches/KernelSU/50_add_susfs_in_gki-android14-6.1.patch"
+    apply_patch_optional \
+        "$KERNEL_REPO/KernelSU-Next" \
+        "$KERNEL_REPO/susfs4ksu/kernel_patches/50_add_susfs_in_gki-android14-6.1.patch"
 
 	apply_patch_optional \
 		"$KERNEL_REPO/KernelSU-Next" \
