831a74b4910a4171ac5af2583c942824c96d96bf
Ante0 <ante00@gmail.com>
2026-07-24 08:21:22 +0200

    Update check-upstream.sh

--- gscripts/check-upstream.sh
+++ gscripts/check-upstream.sh
@@ -88,10 +88,6 @@ fi
 # Current SHAs
 ###############################################################################
 
-RESULT="$(github_commit_info "$KSU_REPO" "$KSU_BRANCH" "kernel")"
-
-printf 'RESULT=%q\n' "$RESULT"
-
 IFS=$'\t' read -r NEW_KSU KSU_MSG \
     < <(github_commit_info "$KSU_REPO" "$KSU_BRANCH" "kernel")
 
