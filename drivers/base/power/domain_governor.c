23cdc8d6d335206158aaace23cb2153e35c4fd87
Ante0 <ante00@gmail.com>
2026-09-17 19:20:48 +0200

    fix domain_governor.c

--- drivers/base/power/domain_governor.c
+++ drivers/base/power/domain_governor.c
@@ -358,7 +358,7 @@ static bool cpu_power_down_ok(struct dev_pm_domain *pd)
 	if (!(genpd->flags & GENPD_FLAG_CPU_DOMAIN))
 		return true;
 
-	global_constraint = cpu_latency_qos_limit();
+	global_constraint = cpu_latency_qos_limit(cpu);
 	/*
 	 * Find the next wakeup for any of the online CPUs within the PM domain
 	 * and its subdomains. Note, we only need the genpd->cpus, as it already
